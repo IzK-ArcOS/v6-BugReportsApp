a108e01eb8911e916391a56e0361804d8396cf7a
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-18 15:37:39 +0100

    Make InfoBlocks a global component

--- Components/Content/Viewer/AdditionalMeta.svelte
+++ Components/Content/Viewer/AdditionalMeta.svelte
@@ -1,37 +1,34 @@
 <script lang="ts">
   import ApiReveal from "$lib/Components/ApiReveal.svelte";
+  import InfoBlock from "$state/Desktop/Components/ProcessRenderer/Window/InfoBlock.svelte";
+  import InfoRow from "$state/Desktop/Components/ProcessRenderer/Window/InfoBlock/InfoRow.svelte";
+  import Segment from "$state/Desktop/Components/ProcessRenderer/Window/InfoBlock/Row/Segment.svelte";
   import { Report } from "$types/bugrep";
 
   export let report: Report;
 </script>
 
-<div class="metadata">
-  <div class="row">
-    <div class="segment">
-      <p class="caption">Provided User Data</p>
-      <p class="value">{report.userdata ? "Yes" : "No"}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Provided Server</p>
-      <p class="value">{report.api ? "Yes" : "No"}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Report ID</p>
-      <p class="value"><ApiReveal api={report.id} /></p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="segment">
-      <p class="caption">Resolved</p>
-      <p class="value">{report.resolved ? "Yes!" : "No..."}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Closed</p>
-      <p class="value">{report.closed ? "Yes" : "No"}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Issue ID</p>
-      <p class="value">{report.issueid}</p>
-    </div>
-  </div>
-</div>
+<InfoBlock>
+  <InfoRow>
+    <Segment title="Provided User Data">
+      {report.userdata ? "Yes" : "No"}
+    </Segment>
+    <Segment title="Provided Server">
+      {report.api ? "Yes" : "No"}
+    </Segment>
+    <Segment title="Report ID">
+      <ApiReveal api={report.id} />
+    </Segment>
+  </InfoRow>
+  <InfoRow>
+    <Segment title="Resolved">
+      {report.resolved ? "Yes!" : "No..."}
+    </Segment>
+    <Segment title="Closed">
+      {report.closed ? "Yes" : "No"}
+    </Segment>
+    <Segment title="Issue ID">
+      {report.issueid}
+    </Segment>
+  </InfoRow>
+</InfoBlock>
--- Components/Content/Viewer/BaseMeta.svelte
+++ Components/Content/Viewer/BaseMeta.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import InfoBlock from "$state/Desktop/Components/ProcessRenderer/Window/InfoBlock.svelte";
+  import InfoRow from "$state/Desktop/Components/ProcessRenderer/Window/InfoBlock/InfoRow.svelte";
+  import Segment from "$state/Desktop/Components/ProcessRenderer/Window/InfoBlock/Row/Segment.svelte";
   import { Logo } from "$ts/branding";
   import { WarningIcon } from "$ts/images/dialog";
   import { Report } from "$types/bugrep";
@@ -8,59 +11,45 @@
   export let report: Report;
 </script>
 
-<div class="metadata">
-  <div class="row">
-    <div class="segment">
-      <p class="caption">Created By</p>
-      <p class="value">{report.author}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Date</p>
-      <p class="value">{dayjs(report.created_at).format("MMMM D YYYY")}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Time</p>
-      <p class="value">{dayjs(report.created_at).format("h:mm A")}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Server</p>
-      <p class="value">{report.api || "Unknown"}</p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="segment">
-      <p class="caption">User Agent</p>
-      <p class="value">{report.useragent || "No user agent provided"}</p>
-    </div>
-  </div>
-  <div class="row">
-    <div class="segment">
-      <p class="caption">Client</p>
-      <p class="value">{report.frontend || "Unknown"}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Vite</p>
-      <p class="value">{report.metaenv.MODE}</p>
-    </div>
-    <div class="segment">
-      <p class="caption">Desktop</p>
-      <p class="value">{report.desktop ? "Yes" : "No"}</p>
-    </div>
-    <div class="segment mode">
-      <p class="caption">Mode</p>
-      <p class="value">
-        <span>{report.mode_file}</span>
-        <img src={Logo(report.mode_file)} alt="" />
-      </p>
-    </div>
-    <div class="segment right version">
-      <p class="caption">Version</p>
-      <p class="value">
-        <span>{report.version}</span>
-        {#if !report.version.startsWith("6.")}
-          <img src={WarningIcon} alt="" />
-        {/if}
-      </p>
-    </div>
-  </div>
-</div>
+<InfoBlock>
+  <InfoRow>
+    <Segment title="Created By">
+      {report.author}
+    </Segment>
+    <Segment title="Date">
+      {dayjs(report.created_at).format("MMMM D YYYY")}
+    </Segment>
+    <Segment title="Time">
+      {dayjs(report.created_at).format("h:mm A")}
+    </Segment>
+    <Segment title="Server">
+      {report.api || "Unknown"}
+    </Segment>
+  </InfoRow>
+  <InfoRow>
+    <Segment title="User Agent">
+      {report.useragent || "No user agent provided"}
+    </Segment>
+  </InfoRow>
+  <InfoRow>
+    <Segment title="Client">
+      {report.frontend || "Unknown"}
+    </Segment>
+    <Segment title="Vite">
+      {report.metaenv.MODE}
+    </Segment>
+    <Segment title="Desktop">
+      {report.desktop ? "Yes" : "No"}
+    </Segment>
+    <Segment title="Mode" className="mode">
+      <span>{report.mode_file}</span>
+      <img src={Logo(report.mode_file)} alt="" />
+    </Segment>
+    <Segment right title="Version" className="version">
+      <span>{report.version}</span>
+      {#if !report.version.startsWith("6.")}
+        <img src={WarningIcon} alt="" />
+      {/if}
+    </Segment>
+  </InfoRow>
+</InfoBlock>
